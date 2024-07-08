d83c0f034bbfb2828ffb11fe1a76ec4e144679aa
Jasur0919 <jasurjohn1111@gmail.com>
2024-07-08 09:28:55 +0500

    order end edit

---
+++ src/router/routes.jsx
@@ -0,0 +1,21 @@
+import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+import HomeIcon from '@mui/icons-material/Home';
+const routes = [
+    {
+        path: "/",
+        content: "Home",
+        icon: <HomeIcon/>
+    },
+    {
+        path: "/services",
+        content: "Service",
+        icon: <LocalPostOfficeIcon/>
+    },
+    {
+        path: "/orders",
+        content: "Order",
+        icon: <LocalPostOfficeIcon/>
+    }
+]
+
+export default routes
